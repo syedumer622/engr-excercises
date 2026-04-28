883417c5df3a297dc99feb3bc4df454b71dd1b6e
Umer <umer@swishtag.com>
2026-04-28 13:57:47 +0500

    Update error message in tierPricing method for clarity on input quantity

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
@@ -9,7 +10,10 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
-    Route::resource('products', \App\Http\Controllers\ProductController::class);
 });
+
+Route::withoutMiddleware('web')->post('/exercise-1-artwork-version', [IndexController::class, 'artworkVersion']); // // exercise = 1
+Route::withoutMiddleware('web')->post('/exercise-2-tier-pricing', [IndexController::class, 'tierPricing']); // exercise = 2
+
 
 require __DIR__.'/settings.php';
