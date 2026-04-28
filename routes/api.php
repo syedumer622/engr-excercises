635a3eb8b9387c56d94c24e64602a82b03411f98
Umer <umer@swishtag.com>
2026-04-28 14:09:03 +0500

    Update error message in tierPricing method for clarity on input quantity

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ use App\Http\Controllers\IndexController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/exercise-1-artwork-version', [IndexController::class, 'artworkVersion']); // // exercise = 1
+Route::post('/exercise-2-tier-pricing', [IndexController::class, 'tierPricing']); // exercise = 2
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
-Route::withoutMiddleware('web')->post('/exercise-1-artwork-version', [IndexController::class, 'artworkVersion']); // // exercise = 1
-Route::withoutMiddleware('web')->post('/exercise-2-tier-pricing', [IndexController::class, 'tierPricing']); // exercise = 2
-
-
 require __DIR__.'/settings.php';
